1b27acecf92f730ab7b01c98e76f99b6b7d67076
Qianglong Mo <sammok2003@hotmail.com>
2014-12-24 01:26:29 +0800

    Create Index page, fill index page.

--- app/app.js
+++ app/app.js
@@ -1,8 +1,27 @@
 // Yunen He 2014 (SukeBeta)
 
-var angularTemplate = angular.module("angularTemplate", ['ngMaterial']);
+var planApp = angular.module("planApp", ['ngMaterial']);
 
-angularTemplate.controller("angularTemplateCtrl", ['$scope','$http', function($scope, $http)
+planApp.controller("planAppCtrl", ['$scope','$http', function($scope, $http)
 {
-	// Something cool here!
+
+}]);
+
+planApp.controller("sidebarCtrl", ['$scope','$mdSidenav', function($scope, $mdSidenav)
+{
+    $scope.toggleLeft = function() {
+        $mdSidenav('left').toggle();
+    };
+
+    $scope.close = function (){
+        $mdSidenav('left').close()
+    };
+}]);
+
+planApp.controller("todoListCtrl", ['$scope', '$http', function($scope, $http)
+{
+    // get todo data;
+    $http.get('data/data.json').success(function (data){
+        $scope.todoData = data;
+    });
 }]);
